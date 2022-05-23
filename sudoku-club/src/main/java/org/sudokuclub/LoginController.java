37f927acf54e50700a4507b2f9e0c2c459130a12
kpagacz <konrad.pagacz@gmail.com>
2022-05-23 23:35:01 +0200

    wip

--- sudoku-club/src/main/java/org/sudokuclub/LoginController.java
+++ sudoku-club/src/main/java/org/sudokuclub/LoginController.java
@@ -1,27 +1,101 @@
 package org.sudokuclub;
 
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.sudokuclub.dao.SudokuDbConnFactory;
+import org.sudokuclub.dao.User;
+import org.sudokuclub.dao.UsersRepository;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Objects;
 
 public class LoginController {
-    @FXML
-    private Label testLabel;
+  @FXML private Button signInButton;
+  @FXML private TextField loginField;
+  @FXML private PasswordField passwordField;
+  @FXML private Label signInMessage;
 
-    public void initialize() {
-        this.testLabel.setText("Login Window");
+  private static class SignInTask extends Task<Boolean> {
+    private final String login;
+    private final String password;
+    private final Connection conn;
+
+    public SignInTask(String login, String password, Connection conn) {
+      this.login = login;
+      this.password = password;
+      this.conn = conn;
     }
 
-    @FXML
-    public void goToRegisterView() {
-        try {
-            Parent newWindow = FXMLLoader.load(App.class.getResource("register-view.fxml"));
-            App.setNewScene(newWindow);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected Boolean call() throws Exception {
+      User user = UsersRepository.get(login, conn);
+      return user.password().equals(password);
     }
+  }
+
+  private void handleSignInSuccess(WorkerStateEvent event) {
+    if ((boolean)event.getSource().getValue()) {
+      System.out.println("Sign In Success");
+      signInMessage.setText("Success");
+    } else {
+      System.out.println("Sign In Failure");
+      signInMessage.setText("Password incorrect");
+    }
+  }
+
+  private void handleSignInFailure(WorkerStateEvent event) {
+    System.out.println("Sign In Failure");
+    signInMessage.setText("Failure");
+    signInButton.setDisable(false);
+  }
+
+  private void handleSignIn(ActionEvent event) {
+    signInButton.setDisable(true);
+    Connection conn = null;
+    try {
+      conn = SudokuDbConnFactory.get();
+      SignInTask signInTask = new SignInTask(loginField.getText(), passwordField.getText(), conn);
+      signInTask.setOnSucceeded(this::handleSignInSuccess);
+      signInTask.setOnFailed(this::handleSignInFailure);
+      Thread thread = new Thread(signInTask);
+      thread.setDaemon(true);
+      thread.start();
+    } catch (Exception e) {
+      signInMessage.setText("Failure");
+      signInButton.setDisable(false);
+    } finally {
+      try {
+        if (conn != null) conn.close();
+      } catch (SQLException e) {
+        signInMessage.setText("Failure");
+        signInButton.setDisable(false);
+      }
+    }
+  }
+
+  @FXML
+  public void initialize() {
+    signInButton.setOnAction(this::handleSignIn);
+  }
+
+  @FXML
+  public void goToRegisterView() {
+    try {
+      Parent newWindow =
+          FXMLLoader.load(Objects.requireNonNull(App.class.getResource("register-view.fxml")));
+      App.setNewScene(newWindow);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
